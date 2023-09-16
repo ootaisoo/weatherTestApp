9d82a6d5adcf0ae4117fe098bb9dc1dee9beddf1
evgenijkonysev <e.konyshev@list.ru>
2023-09-16 16:10:53 +0300

    Layout main fragment.

---
+++ app/src/main/java/com/example/gismeteoapitestapp/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.gismeteoapitestapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+//        TODO() make adecuate routing
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container, MainFragment())
+            .commit()
+    }
+}
