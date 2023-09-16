c5ac102924d23be09f6ea90c9128d185666830c8
evgenijkonysev <e.konyshev@list.ru>
2023-09-16 16:47:31 +0300

    Relayout Date input.

--- app/src/main/java/com/example/gismeteoapitestapp/MainFragment.kt
+++ app/src/main/java/com/example/gismeteoapitestapp/MainFragment.kt
@@ -1,12 +1,19 @@
 package com.example.gismeteoapitestapp
 
 import android.os.Bundle
+import android.text.format.DateUtils
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
+import java.util.Calendar
+
 
 class MainFragment : Fragment() {
+
+    private var pickedDateTime: TextView? = null
+    private val date = Calendar.getInstance()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -14,5 +21,19 @@ class MainFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_main, container, false);
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        pickedDateTime = view.findViewById(R.id.date_picked)
+        setInitialDateTime()
+    }
+
+    private fun setInitialDateTime() {
+        pickedDateTime?.text = DateUtils.formatDateTime(
+            context,
+            date.timeInMillis,
+            DateUtils.FORMAT_SHOW_DATE or DateUtils.FORMAT_SHOW_YEAR
+        )
     }
 }
