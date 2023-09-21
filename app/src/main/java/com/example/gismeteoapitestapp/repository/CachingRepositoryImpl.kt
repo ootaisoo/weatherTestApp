690cd17bbd566b2c65f5b127c618bc6936778c10
evgenijkonysev <e.konyshev@list.ru>
2023-09-21 10:58:30 +0300

    fix save to downloads for api > 29.

--- app/src/main/java/com/example/gismeteoapitestapp/repository/CachingRepositoryImpl.kt
+++ app/src/main/java/com/example/gismeteoapitestapp/repository/CachingRepositoryImpl.kt
@@ -2,13 +2,19 @@ package com.example.gismeteoapitestapp.repository
 
 import android.content.ClipData
 import android.content.ClipboardManager
+import android.content.ContentValues
 import android.content.Context
 import android.os.Build
 import android.os.Environment
+import android.os.storage.StorageManager
+import android.provider.MediaStore
 import android.widget.Toast
+import androidx.annotation.RequiresApi
 import com.example.gismeteoapitestapp.R
 import com.google.gson.Gson
 import java.io.File
+import java.io.PrintWriter
+import java.net.URL
 
 
 class CachingRepositoryImpl(
@@ -33,13 +39,39 @@ class CachingRepositoryImpl(
         }
     }
 
+    @RequiresApi(Build.VERSION_CODES.R)
     override fun saveToDownloads(text: String) {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.Q) {
+            saveToExternalStorage(text)
+        } else {
+            saveFileUsingMediaStore(text)
+        }
+    }
+
+    private fun saveToExternalStorage(text: String) {
         val file = File(Environment.getExternalStoragePublicDirectory(
             Environment.DIRECTORY_DOWNLOADS),
             FORECAST_FILE
         )
 
         file.writeText(gson.toJson(text))
+    }
+
+    @RequiresApi(Build.VERSION_CODES.Q)
+    private fun saveFileUsingMediaStore(text: String) {
+        val contentValues = ContentValues().apply {
+            put(MediaStore.MediaColumns.DISPLAY_NAME, FORECAST_FILE)
+            put(MediaStore.MediaColumns.MIME_TYPE, "text/plain")
+            put(MediaStore.MediaColumns.RELATIVE_PATH, Environment.DIRECTORY_DOWNLOADS)
+        }
+        val resolver = context.contentResolver
+        val uri = resolver.insert(MediaStore.Downloads.EXTERNAL_CONTENT_URI, contentValues)
+        if (uri != null) {
+            resolver.openOutputStream(uri)?.let {
+                it.write(text.toByteArray())
+                it.close()
+            }
+        }
     }
 
     override fun saveRequestsInfo(requestsInfo: String) {
